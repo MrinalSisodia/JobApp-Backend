960166920b223d144b3af5c24c29ecae98111d08
MrinalSisodia <mrinalsisodia28@gmail.com>
2025-11-09 11:53:23 +0530

    comments cleanup

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -7,20 +7,19 @@ const { initializeDatabase } = require("./db.connect");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middleware
+
 app.use(cors()); 
 app.use(express.json());
 
-// Import routes
 const jobRoutes = require("./JobRoutes");
 app.use("/jobs", jobRoutes);
 
-// Base route
+
 app.get("/", (req, res) => {
   res.send("Job Posting API is running...");
 });
 
-// Initialize database and start server
+
 initializeDatabase()
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
